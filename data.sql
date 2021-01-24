e4146584517a482a6c2dd2b6b1d637ee52c44211
Caleb Matteis <matteismusic@gmail.com>
2021-01-24 07:03:28 -0500

    created books database

---
+++ data.sql
@@ -0,0 +1,18 @@
+-- create books data base
+DROP DATABASE IF EXISTS books;
+
+CREATE DATABASE books;
+
+\c books;
+
+
+CREATE TABLE books (
+  isbn TEXT PRIMARY KEY,
+  amazon_url TEXT,
+  author TEXT,
+  language TEXT, 
+  pages INTEGER,
+  publisher TEXT,
+  title TEXT, 
+  year INTEGER
+);
